0be76d387a5ce640808150a0a4a48a4070245bb7
test <test@test.ru>
2024-04-21 01:47:01 +0300

    подключил about.html и contact.html

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexShopView
+from .views import IndexShopView, AboutSopView, ContactSopView
 
 app_name = "home"
 
 urlpatterns = [
     path('', IndexShopView.as_view(), name="index"),
+    path('about/', AboutSopView.as_view(), name="about"),
+    path('contact/', ContactSopView.as_view(), name="contact"),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ class IndexShopView(View):
 
     def get(self, requesst):
         return render(requesst, 'home/index.html')
+
+
+class AboutSopView(View):
+    def get(self, request):
+        return render(request, 'home/about.html')
+
+
+class ContactSopView(View):
+    def get(self, request):
+        return render(request, 'home/contact.html')
