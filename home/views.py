2b668f952bb0d3155e68624dd3241cb9c021cc32
test <test@test.ru>
2024-04-22 03:09:09 +0300

    созданы приложения Shop, Blog,Cart,Checkout, подключены html страницы

--- home/views.py
+++ home/views.py
@@ -3,7 +3,6 @@ from django.views import View
 
 
 class IndexShopView(View):
-
     def get(self, requesst):
         return render(requesst, 'home/index.html')
 
--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'cart',
+    'blog',
+    'checkout',
+    'shop',
 ]
 
 MIDDLEWARE = [
