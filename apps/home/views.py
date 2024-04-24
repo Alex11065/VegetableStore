c1b86c4bfe85ed0fc0b901544f6622a5af9f9aa7
test <test@test.ru>
2024-04-24 11:04:57 +0300

    Был изменен index, товары подаються динамически

--- apps/home/views.py
+++ apps/home/views.py
@@ -4,11 +4,40 @@ from django.views import View
 
 class IndexShopView(View):
     def get(self, requesst):
-        context = {'data': [{'name': 'Bell Peper',
+        context = {'data': [{'name': 'Болгарский перец',
                              'discount': 30,
                              'price_before': 120.00,
                              'price_after': 80.00,
-                             'url': 'store/images/product-1.jpg'}
+                             'url': 'store/images/product-1.jpg'},
+                            {'name': 'Клубника',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-2.jpg'},
+                            {'name': 'Зеленая фасоль',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-3.jpg'},
+                            {'name': 'Фиолетовая капуста',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-4.jpg'},
+                            {'name': 'Томаты',
+                             'discount': 30,
+                             'price_before': 120.00,
+                             'price_after': 80.00,
+                             'url': 'store/images/product-5.jpg'},
+                            {'name': 'Брокколи',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-6.jpg'},
+                            {'name': 'Морковь',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-7.jpg'},
+                            {'name': 'Фруктовый сок',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-8.jpg'},
                             ]
                    }
         return render(requesst, 'home/index.html', context)
