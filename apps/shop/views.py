ae2d5b3be7328eb1ad8c3929573f2fcad2e7b9e2
test <test@test.ru>
2024-04-24 12:25:45 +0300

    Был изменен файл shop, товары подаються динамически

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -3,8 +3,60 @@ from django.views import View
 
 
 class ShopView(View):
-    def get(self, request):
-        return render(request, 'shop/shop.html')
+    def get(self, requesst):
+        context = {'data': [{'name': 'Болгарский перец',
+                             'discount': 30,
+                             'price_before': 120.00,
+                             'price_after': 80.00,
+                             'url': 'store/images/product-1.jpg'},
+                            {'name': 'Клубника',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-2.jpg'},
+                            {'name': 'Зеленая фасоль',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-3.jpg'},
+                            {'name': 'Фиолетовая капуста',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-4.jpg'},
+                            {'name': 'Томаты',
+                             'discount': 30,
+                             'price_before': 120.00,
+                             'price_after': 80.00,
+                             'url': 'store/images/product-5.jpg'},
+                            {'name': 'Брокколи',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-6.jpg'},
+                            {'name': 'Морковь',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-7.jpg'},
+                            {'name': 'Фруктовый сок',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-8.jpg'},
+                            {'name': 'Лук',
+                             'discount': None,
+                             'price_before': 100.00,
+                             'url': 'store/images/product-9.jpg'},
+                            {'name': 'Яблоки',
+                             'discount': None,
+                             'price_before': 110.00,
+                             'url': 'store/images/product-10.jpg'},
+                            {'name': 'Чеснок',
+                             'discount': None,
+                             'price_before': 90.00,
+                             'url': 'store/images/product-11.jpg'},
+                            {'name': 'Перец чили',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-12.jpg'},
+                            ]
+                   }
+        return render(requesst, 'shop/shop.html', context)
 
 
 class ProductSingleView(View):
